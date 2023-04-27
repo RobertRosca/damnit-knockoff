d896fd27df421fa29f6c2aef908c161447756d01
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2023-04-27 11:43:51 +0200

    Add comments to context

--- context.py
+++ context.py
@@ -2,16 +2,28 @@ import random
 
 from damnit_knockoff.context_reader import BaseRun
 
+# Users can define methods, which are not called but can be used in the classes
+def some_method() -> int:
+    return random.randint(0, 100)
 
+# Users can define classes, which are used to create documents (tables) in mongo
 class SomeTable(BaseRun):
+    # Class methods are parsed and used to create fields in the document, e.g
+    # def foo(self) -> int will create a field called foo with type int
+    # Methods are called before the document is inserted into the database
     def foo(self) -> int:
-        print("bonk")
-        return random.randint(0, 100)
+        return some_method()
 
-    def bar(self) -> bool:
+    # Async methods are also supported
+    async def bar(self) -> bool:
         return random.randint(0, 1) == 1
 
+    def oof(self) -> str | None:
+        if self.bar:
+            return "hello"
 
+# Users can define multiple classes in the same file, and they will all be used
+# to create multiple tables in the database
 class MultipleTablesWorkAccidentallyIGuess(BaseRun):
     def buzz(self) -> int:
-        return 100
+        return some_method()
