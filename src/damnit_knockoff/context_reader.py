2d536119e1a58ae4604829c2c125d2bfb2464aeb
Robert Rosca <roscar@max-exfl015.desy.de>
2023-04-28 11:20:47 +0200

    Re-implement code I forgot to push

--- src/damnit_knockoff/context_reader.py
+++ src/damnit_knockoff/context_reader.py
@@ -1,6 +1,8 @@
+import asyncio
 import importlib.util
 import inspect
-from typing import Type
+from types import FunctionType
+from typing import Any, Type
 
 from beanie import Document, Insert, before_event
 from pydantic import Extra, create_model
@@ -15,7 +17,8 @@ class BaseRun(Document, extra=Extra.allow):
 def load_user_context():
     """Load the context file as a module."""
     spec = importlib.util.spec_from_file_location(
-        "context", "/home/roscar/work/git.xfel.eu/roscar/damnit-knockoff/context.py"
+        "context",
+        "/home/roscar/work/github.com/RobertRosca/damnit-knockoff/context.py",
     )
 
     assert spec is not None
@@ -27,11 +30,23 @@ def load_user_context():
     return module
 
 
+def pseudo_property_decorator(func):
+    async def wrapper(self, *args, **kwargs):
+        if asyncio.iscoroutinefunction(func):
+            val = await func(self, *args, **kwargs)
+        else:
+            val = func(self, *args, **kwargs)
+
+        setattr(self, func.__name__, val)
+
+    return wrapper
+
+
 def parse_methods_as_fields(cls: Type[Document]):
     """Read class methods, prepend "def_" to their name, using the return type
     annotation of the method create a field with the same name and type."""
-    fields = {}
-    methods = {}
+    fields: dict[str, tuple[Type, Any]] = {}
+    methods: dict[str, FunctionType] = {}
 
     for method in inspect.getmembers(cls, inspect.isfunction):
         if method[0].startswith("_") or method in inspect.getmembers(
@@ -47,14 +62,13 @@ def parse_methods_as_fields(cls: Type[Document]):
         cls.__name__,
         __base__=cls.__base__,
         __module__=cls.__module__,
-        # __validators__=cls.__validators__,
-        # **cls.__dict__["__annotations__"],
         **fields,
     )
 
-    for method in methods:
-        decorated = before_event(Insert)(methods[method])
-        setattr(new_cls, method, decorated)
+    for name, method in methods.items():
+        decorated = pseudo_property_decorator(method)
+        decorated = before_event(Insert)(decorated)
+        setattr(new_cls, name, decorated)
 
     return new_cls
 
