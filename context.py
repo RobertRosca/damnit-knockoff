1ba18f22fc127a4e5fc244ad38e9920ced68f756
Robert Rosca <roscar@max-exfl014.desy.de>
2023-04-30 13:12:42 +0200

    Lose the plot

--- context.py
+++ context.py
@@ -1,26 +1,31 @@
-import random
-
-from beanie import after_event
+import datetime
+from pathlib import Path
 
 from damnit_knockoff.context_reader import BaseRun, field
+import plotly.express as px
+
+
+import random
 
 
 class SomeTable(BaseRun):
-    def method(self):
-        print("bonk")
+    @field
+    def created_at(self) -> datetime.datetime:
+        return datetime.datetime.now()
 
     @field
-    def foo(self) -> int:
-        print("foo")
-        return random.randint(0, 100)
+    def files_in_raw(self) -> int:
+        return len(list((self.path / "raw" / f"r{self.run:04d}").glob("*")))
 
-    @field(direction=after_event)
-    def bar(self) -> bool:
-        print("bar")
-        return random.randint(0, 1) == 1
-
-
-class MultipleTablesWorkAccidentallyIGuess(BaseRun):
     @field
-    def buzz(self) -> int:
-        return 100
+    def raw_run_dir(self) -> Path:
+        return self.path / "raw" / f"r{self.run:04d}"
+
+    @field
+    def random_list(self) -> list | None:
+        return [{"x": str(x), "y": random.random() * x} for x in range(1, 100)]
+
+    @field
+    def html_plot(self) -> str | None:
+        fig = px.scatter(x=range(100), y=[random.random() * x for x in range(100)])
+        return fig.to_html(include_plotlyjs="cdn")
