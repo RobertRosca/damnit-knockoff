215bf5c8cd588d5d57a541e6d81f56bb02b835b2
Robert Rosca <robert.rosca@xfel.eu>
2023-05-03 19:30:38 +0200

    Tidy up test script

--- test.py
+++ test.py
@@ -7,15 +7,18 @@ from damnit_knockoff.context_reader import MODELS
 
 
 async def test():
-    await db_init()
+    await db_init(
+        state=None,  # type: ignore
+        url="mongodb://localhost:27017/",
+    )
 
     for model in MODELS:
-        latest = await model.find().sort(-model.run).limit(1).first_or_none()
-        await model(
+        latest = await model.find().sort(-model.run).limit(1).first_or_none()  # type: ignore
+        res = await model(
             proposal=4696,
             run=latest.run + 1 if latest else 1,
             path=Path("/gpfs/exfel/exp/HED/202321/p004696/"),
-        ).insert()
+        ).insert()  # type: ignore
 
 
 if __name__ == "__main__":
